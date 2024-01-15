575d33e8b8cfc9067c1f73cb749015e34bd60a09
Klucly <klucly05@gmail.com>
2024-01-16 01:37:00 +0200

    Now we're going somewhere

--- main.py
+++ main.py
@@ -1,31 +1,16 @@
-import socket  # Importing the socket module to create network sockets
+from flask import Flask, request
 
-# Defining the host and port to listen on
-HOST = '0.0.0.0'  # Localhost IP address
-PORT = '10000'  # Port number to listen on
+app = Flask(__name__)
 
-# Creating a socket object for the server
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# @app.route('/')
+# def index():
+# 	return 'Hello World!'
 
-# Binding the socket object to the host and port
-server.bind((HOST, int(PORT)))
+@app.route('/', methods=['POST'])
+def result():
+    print(request.data)  # raw data
+    print(request.json)  # json (if content-type of application/json is sent with the request)
+    print(request.get_json(force=True))  # json (if content-ty
 
-# Listening for incoming connections from clients, with a maximum queue of 5 clients
-server.listen(5)
-
-# Infinite loop to keep the server running
-while True:
-    # Accepting incoming connection from client and getting the communication socket and client address
-    communication_socket, address = server.accept()
-    print(f"connected to {address}")
-
-    # Receiving data from the client and decoding it to a string using UTF-8 encoding
-    message = communication_socket.recv(1024).decode('utf-8')
-    print(f"message: {message}")
-
-    # Sending a message back to the client to acknowledge that the message was received
-    communication_socket.send(f"message received".encode('utf-8'))
-
-    # Closing the communication socket with the client
-    communication_socket.close()
-    print(f"communication with {address} ended")
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=10000, debug=False)
--- socket_client.py
+++ socket_client.py
@@ -1,8 +1,19 @@
 import socket  # Importing the socket module to create network sockets
 
 # Defining the host and port to connect to
-HOST = '35.157.117.28'  # Localhost IP address
-PORT = '8080'  # Port number to connect to
+HOST = '127.0.0.1'  # Localhost IP address
+PORT = '10000'  # Port number to connect to
+
+data = '''POST http://localhost:10000/ HTTP/1.1
+Host: localhost:10000
+Content-Length: 20
+Content-Type: application/json
+Accept-Encoding: gzip, deflate, compress
+Accept: */*
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36
+
+{"helow": "roworor"}'''
+
 
 # Creating a socket object for the client
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,10 +22,10 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST, int(PORT)))
 
 # Sending a message to the server by encoding a string to bytes using UTF-8 encoding and sending it through the socket
-client_socket.send("Hello world!".encode('utf-8'))
+client_socket.send(data.encode('utf-8'))
 
 # Receiving a response from the server by receiving up to 1024 bytes of data through the socket and decoding it to a string using UTF-8 encoding
-print(client_socket.recv(1024))
+# print(client_socket.recv(1024))
 
 # Closing the socket connection with the server
 client_socket.close()
